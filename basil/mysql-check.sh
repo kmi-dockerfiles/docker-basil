2b0166efafc05a024713eb145efe5841d16907cc
Panziera, Luca <luca.panziera@ft.com>
2016-03-25 15:28:18 +0000

    Fixed mysql wait by Basil container

--- basil/mysql-check.sh
+++ basil/mysql-check.sh
@@ -1,5 +1,7 @@
 #!/bin/bash  
 # checking if the basil db is configured
+echo "Waiting for Mysql startup..." 
+sleep 10
 echo 'Checking, if the database is empty or not.';
 count="select count(*) from information_schema.tables where table_type = 'BASE TABLE' and table_schema = 'basil'"
 mysql -h db -ubasil -ppV7JuwTj7FAjDG3B basil -e "$count" > mysql_status.txt 
