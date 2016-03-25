ddcfd172ce4da676636080325152175626dfd9b1
Panziera, Luca <luca.panziera@ft.com>
2016-03-25 01:35:27 +0000

    Created Dockerfile for Pesto

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM java:8
-RUN mkdir /basil 
-COPY basil.ini db.sql log4j2.xml run.sh basil-server-0.3.2.jar /basil/
-WORKDIR /basil/
-RUN apt-get -y update && apt-get install -y mysql-client 
-CMD ["/run.sh"]
---
+++ basil/mysql-check.sh
@@ -0,0 +1,13 @@
+#!/bin/bash  
+# checking if the basil db is configured
+echo 'Checking, if the database is empty or not.';
+count="select count(*) from information_schema.tables where table_type = 'BASE TABLE' and table_schema = 'basil'"
+mysql -h db -ubasil -ppV7JuwTj7FAjDG3B basil -e "$count" > mysql_status.txt 
+stat=`cat mysql_status.txt | tail -1`
+rm -rf mysql_status.txt
+if [ "$stat" != "0" ]; then
+	echo 'The database specified, is not empty.';
+else
+    echo 'Database is empty... creating db'
+    mysql -h db -ubasil -ppV7JuwTj7FAjDG3B basil < db.sql 
+fi
